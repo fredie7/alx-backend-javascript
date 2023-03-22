596428d72e52533486cff0faf0d837f4735e94fe
fredie7 <52379604+fredie7@users.noreply.github.com>
2023-03-22 18:37:32 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,9 @@
-import signUpUser from './4-all-reject';
-import uploadPhoto from './5-all-reject';
+// Handle multiple promises
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const res = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    });
-  }
-  return res;
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
+
+export default handleProfileSignup;
